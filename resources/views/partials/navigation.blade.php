792efa1996bc25b339d841f3081d90ba3ad3a0e3
James Boyle <james.patrick.boyle92@gmail.com>
2024-05-04 11:02:52 +0200

    Update nav toggle functionality

--- resources/views/partials/navigation.blade.php
+++ resources/views/partials/navigation.blade.php
@@ -1,10 +1,10 @@
-<nav id="nav" class="fixed p-4 right-0 w-[300px] top-0 bottom-0 bg-black bg-opacity-90 md:shadow-xl z-[100] flex flex-col gap-8">
+<nav id="nav" class="nav-closed fixed p-4 right-0 w-[300px] top-0 bottom-0 bg-black bg-opacity-90 md:shadow-xl z-[100] flex flex-col gap-8">
 
-  <div class="h-10 w-10 text-red-500 grid items-center">
+  <div class="grid items-center w-10 h-10 text-red-500">
     <span id="closeNav" class="text-xl font-bold">X</span>
   </div>
 
-  <div class="flex-1 space-y-2 gap-2 justify-end block">
+  <div class="justify-end flex-1 block gap-2 space-y-2">
     <a href="{{route('home')}}" class="text-white p-1 md:p-2 block text-xl border border-transparent hover:border-white hover:scale-105 transition-all rounded-sm {{ Route::is('home') ? 'bg-red-500' : '' }}">
       Home
     </a>
