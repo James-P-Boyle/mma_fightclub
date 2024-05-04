3d83c9bccbc16ac441f0652778d1578fa19feaff
James Boyle <james.patrick.boyle92@gmail.com>
2024-05-04 11:08:22 +0200

    Restyle footer

--- resources/js/navigation.js
+++ resources/js/navigation.js
@@ -12,4 +12,3 @@ const toggleNav = () => {
  openNav.addEventListener('click', toggleNav);
  closeNav.addEventListener('click', toggleNav);
 
-//<nav id="nav" class="fixed p-4 right-0 w-[300px] top-0 bottom-0 bg-black bg-opacity-90 md:shadow-xl z-[100] flex flex-col gap-8">
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,13 +1,16 @@
-<footer class="">
-  <div class="flex flex-col items-center justify-center gap-4 border-t py-6 dark:border-0">
+<footer class="bg-gray-100">
+  <div class="flex flex-row items-center justify-center gap-4 py-8 border-t">
    
-    <a href="{{ route('home') }}" class="transition duration-100 hover:text-ci-black active:text-ci-black {{ Route::is('home') ? 'underline' : '' }}">
-        Home
+    <a href="{{ route('home') }}" class="transition duration-100 text-lg {{ Route::is('home') ? 'underline text-red-500' : '' }}">
+      Home
+    </a>
+    <a href="{{ route('contact') }}" class="transition duration-100 text-lg {{ Route::is('contact') ? 'underline text-red-500' : '' }}">
+      Contact
     </a>
 
   </div>
 
-  <div class="py-6 max-w-md mx-auto text-center text-sm text-ci-black dark:text-ci-white border-t dark:border-0">
+  <div class="max-w-md py-8 mx-auto text-sm text-center border-t text-ci-black">
     <span>© 2024 - Sander Gebrauchte Reifen. All rights reserved.</span>
   </div>
 </footer>
