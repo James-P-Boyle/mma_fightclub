e49008f8b1838583178b0809fced491522989e31
James Boyle <james.patrick.boyle92@gmail.com>
2024-05-30 11:59:18 +0200

    Fix mixed content by serving assets over HTTPS

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,23 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    build: {
+        outDir: 'public/build',
+        assetsDir: '',
+        sourcemap: false,
+        minify: true,
+        rollupOptions: {
+            input: '/index.html'
+        }
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
+    server: {
+        https: true
+    },
     base: process.env.APP_URL || '/',
 });
