2d0c2890c831cf23ee2eb2a8d03628013d2bc078
James Boyle <james.patrick.boyle92@gmail.com>
2024-06-02 19:27:24 +0200

    Update highlight cards

--- resources/views/sections/highlights.blade.php
+++ resources/views/sections/highlights.blade.php
@@ -1,24 +1,13 @@
 <section class="flex flex-col justify-center gap-8 px-4">
 
-  <div class="flex flex-col gap-4 md:flex-row justify-center items-center md:max-w-[12000px] mx-auto">
+  <div class="flex flex-col gap-8 lg:flex-row justify-center items-center lg:max-w-[12000px] mx-auto">
 
     <a 
         href="{{route('mma')}}" 
         class="p-4 text-center border border-gray-100 hover:border-gray-200 hover:shadow-lg rounded-xl hover:scale-105 hover:cursor-pointer transtion-all group" 
         title="Learn More About Our MMA Classes and Training at MMA Fight Club Bad Hersfeld"
     >
-        <span class="inline-block p-2 rounded-lg ">
-            <svg
-                class="w-16 h-16"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-            >
-            <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
-            <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"></path>
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"></path></svg>
-        </span>
+        <img src="images/icon_mma.png" alt="" class="w-60 h-60">
     
         <p class="text-lg font-bold md:text-xl group-hover:text-ci-red">Mixed Martial Arts</p>
     </a>
@@ -27,18 +16,7 @@
         class="p-4 text-center border border-gray-100 hover:border-gray-200 hover:shadow-lg rounded-xl hover:scale-105 hover:cursor-pointer transtion-all group" 
         title="Learn More About Our Brazillian Jiu Jitsu Classes and Training at MMA Fight Club Bad Hersfeld"
     >
-        <span class="inline-block p-2 rounded-lg ">
-            <svg
-                class="w-16 h-16"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-            >
-            <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
-            <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"></path>
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"></path></svg>
-        </span>
+        <img src="images/icon_jitsu.png" alt="" class="w-60 h-60">
     
         <p class="text-lg font-bold md:text-xl group-hover:text-ci-red">Brazillian Jiu Jitsu</p>
     </a>
@@ -48,18 +26,7 @@
         class="p-4 text-center border border-gray-100 hover:border-gray-200 hover:shadow-lg rounded-xl hover:scale-105 hover:cursor-pointer transtion-all group" 
         title="Learn More About Our Muay Thai Classes and Training at MMA Fight Club Bad Hersfeld"
     >
-        <span class="inline-block p-2 rounded-lg ">
-            <svg
-                class="w-16 h-16"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-            >
-            <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
-            <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"></path>
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"></path></svg>
-        </span>
+        <img src="images/icon_muay_thai.png" alt="" class="w-60 h-60">
     
         <p class="text-lg font-bold md:text-xl group-hover:text-ci-red">Muay Thai</p>
     </a>
@@ -69,22 +36,11 @@
         class="p-4 text-center border border-gray-100 hover:border-gray-200 hover:shadow-lg rounded-xl hover:scale-105 hover:cursor-pointer transtion-all group" 
         title="Learn More About Our Grappling Classes and Training at MMA Fight Club Bad Hersfeld"
     >
-        <span class="inline-block p-2 rounded-lg ">
-            <svg
-                class="w-16 h-16"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-            >
-            <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
-            <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"></path>
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"></path></svg>
-        </span>
+        <img src="images/icon_wrestling.png" alt="" class="w-60 h-60">
     
         <p class="text-lg font-bold md:text-xl group-hover:text-ci-red">Grappling</p>
     </a>
-
+{{-- 
     <a 
         href="{{route('kids-jitsu')}}" 
         class="p-4 text-center border border-gray-100 hover:border-gray-200 hover:shadow-lg rounded-xl hover:scale-105 hover:cursor-pointer transtion-all group" 
@@ -104,7 +60,7 @@
         </span>
     
         <p class="text-lg font-bold md:text-xl group-hover:text-ci-red">Kids Brazillian Jiu-Jitsu</p>
-    </a>
+    </a> --}}
 
   </div>
 </section>
